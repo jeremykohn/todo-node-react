64e0bec9836dafddccce32584ed68579a3d76756
Jeremy Kohn <jeremykohndev@gmail.com>
2021-09-13 00:03:20 -0500

    Create basic app layout

--- frontend/todo/src/App.js
+++ frontend/todo/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './normalize.css';
+import './ds-grid.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="col sidebar">
+        <h4>Add Todo</h4>
+        <form onSubmit="{}">
+          <ul>
+            <li>
+              Description: <input type="text" />
+            </li>
+            <li>
+              Upload attachment: <input type="file" />
+            </li>
+            <li>
+              Deadline: <input type="datetime-local" />
+            </li>
+            <li>
+              <input type="submit" />
+            </li>
+          </ul>
+        </form>
+      </div>
+      <div className="col todoListItems">
+        <h3>Todo List</h3>
+        <h4>Active Tasks</h4>
+        <ul>
+          <li><input type="checkbox" onClick="move to completed items" /> -Description- -Attachment- -Deadline- </li>
+          ...one li per todo item...
+        </ul>
+        <hr />
+        <h4>Completed Tasks</h4>
+        <ul>
+          <li><input type="checkbox" onClick="nothing" /> -Description- -Attachment- -Deadline- </li>
+          ...one li per todo item...
+        </ul>
+        <hr />
+        <h4>Dismissed Tasks</h4>
+        <ul>
+          <li><input type="checkbox" onClick="nothing" /> -Description- -Attachment- -Deadline- </li>
+          ...one li per todo item...
+        </ul>
+        
+
+      </div>
     </div>
   );
 }
